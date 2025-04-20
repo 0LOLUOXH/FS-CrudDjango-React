4f818da544157b83d759051f6cd29e580519a3ec
0LOLUOXH <83889175+0LOLUOXH@users.noreply.github.com>
2025-04-19 22:35:12 -0600

    tailwind

--- fs-front/src/pages/producto.jsx
+++ fs-front/src/pages/producto.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom'
 import {Tabla} from '../components/tabla'
+import { Inventario } from '../components/inventario'
 
 function producto (){
     return (
         <div>
             <Link to="/addproducto">
-                <h1>
+                <h1 class="bg-indigo-600 hover:not-focus:bg-indigo-700">
                     Agregar Producto
                 </h1>
             </Link>
+            <Inventario />
             <Tabla />
         </div>
     )
