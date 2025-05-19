1da64ed6c83a49c1e2dda7288aa1f37bc9d80a0b
Kuramavega <kuramavega17@gmail.com>
2025-05-18 23:18:39 -0600

    nav frontend

--- fs-front/src/components/navegation.jsx
+++ fs-front/src/components/navegation.jsx
@@ -130,138 +130,124 @@ export function Navegation() {
           </button>
         </div>
 
-        <nav className="mt-20 flex flex-col px-2">
-          <Link
-            to="/"
-            className={`${linkBase} ${location.pathname === "/" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 10l9-7 9 7v11a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2v-6h-4v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V10z" />
-            </svg>
-            Inicio
-          </Link>
-          <Link
-            to="/compras"
-            className={`${linkBase} ${location.pathname === "/compras" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2.4l.9 4.5H21l-1.8 9H6.3l-.9-4.5H3V3zM7 21a2 2 0 100-4 2 2 0 000 4zm10 0a2 2 0 100-4 2 2 0 000 4z" />
-            </svg>
-            Compras
-          </Link>
-          <Link
-            to="/ventas"
-            className={`${linkBase} ${location.pathname === "/ventas" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8c-1.105 0-2 .672-2 1.5S10.895 11 12 11s2 .672 2 1.5S13.105 14 12 14M12 4v2m0 8v2" />
-            </svg>
-            Ventas
-          </Link>
-          <Link
-            to="/producto"
-            className={`${linkBase} ${location.pathname === "/producto" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 7l-8-4-8 4v11a1 1 0 001 1l7-4 7 4a1 1 0 001-1V7z" />
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v18" />
-            </svg>
-            Producto
-          </Link>
-          <Link
-            to="/marcasymodelos"
-            className={`${linkBase} ${location.pathname === "/marcasymodelos" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 7l-8-4-8 4v11a1 1 0 001 1l7-4 7 4a1 1 0 001-1V7z" />
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v18" />
-            </svg>
-            Marcas y Modelos
-          </Link>
-          <Link
-            to="/clientes"
-            className={`${linkBase} ${location.pathname === "/clientes" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a4 4 0 00-3-3.87M9 20H4v-2a4 4 0 013-3.87" />
-              <circle cx="12" cy="7" r="4" stroke="#FEBA53" strokeWidth={2} fill="none" />
-            </svg>
-            Clientes
-          </Link>
-          <Link
-            to="/bodega"
-            className={`${linkBase} ${location.pathname === "/bodega" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7h18v4H3z" />
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 11v8a1 1 0 001 1h12a1 1 0 001-1v-8" />
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 3h6v4H9z" />
-            </svg>
-            Bodega
-          </Link>
-          <Link
-            to="/proveedores"
-            className={`${linkBase} ${location.pathname === "/proveedores" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 17H5a2 2 0 00-2-2V9h18v6a2 2 0 00-2 2h-4M7 9V5h10v4" />
-              <circle cx="7" cy="17" r="2" stroke="#FEBA53" strokeWidth={2} fill="none" /><circle cx="17" cy="17" r="2" stroke="#FEBA53" strokeWidth={2} fill="none" />
-            </svg>
-            Proveedor
-          </Link>
-          <Link
-            to="/empleados"
-            className={`${linkBase} ${location.pathname === "/empleados" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a4 4 0 00-3-3.87M9 20H4v-2a4 4 0 013-3.87" />
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 12a4 4 0 100-8 4 4 0 000 8z" />
-            </svg>
-            Empleados
-          </Link>
-          <Link
-            to="/inventario"
-            className={`${linkBase} ${location.pathname === "/inventario" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5h6a2 2 0 012 2v12l-4-4-4 4V7a2 2 0 012-2z" />
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 9h6M9 13h6M9 17h6" />
-            </svg>
-            Inventario
-          </Link>
-          <Link
-            to="/historialventa"
-            className={`${linkBase} ${location.pathname === "/historialventa" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3v18h18" />
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 9l-4 4-4-4" />
-            </svg>
-            Historial de ventas
-          </Link>
-          <Link
-            to="/historialcompra"
-            className={`${linkBase} ${location.pathname === "/historialcompra" ? linkActive : linkInactive}`}
-            onClick={() => setOpen(false)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 mr-3 text-[#FEBA53]">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 14l2 2 4-4" />
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 2H9a2 2 0 00-2 2v16l4-4 4 4V4a2 2 0 00-2-2z" />
-            </svg>
-            Historial de compras
-          </Link>
-        </nav>
+<nav className="mt-20 flex flex-col px-2">
+  {/* Inicio queda al principio */}
+  <Link
+    to="/"
+    className={`${linkBase} ${location.pathname === "/" ? linkActive : linkInactive}`}
+    onClick={() => setOpen(false)}
+  >
+    Inicio
+  </Link>
+
+  {/* + Compra */}
+  <details className="w-full" open>
+    <summary className={`${linkBase} ${linkInactive} cursor-pointer select-none`}>
+      + Compra
+    </summary>
+    <div className="ml-4">
+      <Link
+        to="/compras"
+        className={`${linkBase} ${location.pathname === "/compras" ? linkActive : linkInactive}`}
+        onClick={() => setOpen(false)}
+      >
+        – Registrar compra
+      </Link>
+      <Link
+        to="/historialcompra"
+        className={`${linkBase} ${location.pathname === "/historialcompra" ? linkActive : linkInactive}`}
+        onClick={() => setOpen(false)}
+      >
+        – Historial de compras
+      </Link>
+    </div>
+  </details>
+
+  {/* + Ventas */}
+  <details className="w-full">
+    <summary className={`${linkBase} ${linkInactive} cursor-pointer select-none`}>
+      + Ventas
+    </summary>
+    <div className="ml-4">
+      <Link
+        to="/ventas"
+        className={`${linkBase} ${location.pathname === "/ventas" ? linkActive : linkInactive}`}
+        onClick={() => setOpen(false)}
+      >
+        – Registrar venta
+      </Link>
+      <Link
+        to="/historialventa"
+        className={`${linkBase} ${location.pathname === "/historialventa" ? linkActive : linkInactive}`}
+        onClick={() => setOpen(false)}
+      >
+        – Historial de ventas
+      </Link>
+    </div>
+  </details>
+
+  {/* + Producto */}
+  <details className="w-full">
+    <summary className={`${linkBase} ${linkInactive} cursor-pointer select-none`}>
+      + Producto
+    </summary>
+    <div className="ml-4">
+      <Link
+        to="/producto"
+        className={`${linkBase} ${location.pathname === "/producto" ? linkActive : linkInactive}`}
+        onClick={() => setOpen(false)}
+      >
+        – Ingresar producto
+      </Link>
+      <Link
+        to="/marcasymodelos"
+        className={`${linkBase} ${location.pathname === "/marcasymodelos" ? linkActive : linkInactive}`}
+        onClick={() => setOpen(false)}
+      >
+        – Marcas y modelos
+      </Link>
+      <Link
+        to="/bodega"
+        className={`${linkBase} ${location.pathname === "/bodega" ? linkActive : linkInactive}`}
+        onClick={() => setOpen(false)}
+      >
+        – Bodega
+      </Link>
+      <Link
+        to="/inventario"
+        className={`${linkBase} ${location.pathname === "/inventario" ? linkActive : linkInactive}`}
+        onClick={() => setOpen(false)}
+      >
+        – Inventario
+      </Link>
+      <span className={linkBase}>– Stock</span>
+    </div>
+  </details>
+
+  {/* Resto de enlaces directos */}
+  <Link
+    to="/clientes"
+    className={`${linkBase} ${location.pathname === "/clientes" ? linkActive : linkInactive}`}
+    onClick={() => setOpen(false)}
+  >
+    Clientes
+  </Link>
+  <Link
+    to="/proveedores"
+    className={`${linkBase} ${location.pathname === "/proveedores" ? linkActive : linkInactive}`}
+    onClick={() => setOpen(false)}
+  >
+    Proveedor
+  </Link>
+  <Link
+    to="/empleados"
+    className={`${linkBase} ${location.pathname === "/empleados" ? linkActive : linkInactive}`}
+    onClick={() => setOpen(false)}
+  >
+    Empleados
+  </Link>
+</nav>
+
       </div>
     </>
   );
